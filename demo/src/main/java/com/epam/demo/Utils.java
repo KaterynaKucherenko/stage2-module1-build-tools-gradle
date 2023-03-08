2d04a83b693dadab1c7e9026d31a42284d837851
KaterynaKucherenko <estorskaya@gmail.com>
2023-03-08 18:35:34 +0300

    commit1

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -5,5 +5,13 @@ import java.util.List;
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
         //magic happens here
-    }
-}
+        boolean result = true;
+        for(String i: args){
+     if  (Integer.parseInt(i)>0){
+          result = true;}
+         else {
+             result = false;
+         }
+     }
+        return result;
+}}
